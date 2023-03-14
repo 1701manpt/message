5630021f0b075d06bc9a8090ace43a606dfb7ccc
1701manpt <thaiphuongnam1071@gmail.com>
2023-03-14 16:21:42 +0700

    .

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+    });
+});
+
+http.listen(3000, () => {
+    console.log('http://localhost:3000/');
+});
